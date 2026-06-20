c6882f8e8541ca3991255ae31c620ac60b810934
mohankumaronly <mohankumaronly81@gmail.com>
2026-06-20 18:11:15 +0530

    database connection is done

--- Backend/src/main/java/com/resume/builder/BuilderApplication.java
+++ Backend/src/main/java/com/resume/builder/BuilderApplication.java
@@ -1,11 +1,21 @@
 package com.resume.builder;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuilderApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(BuilderApplication.class, args);
 	}
 }
